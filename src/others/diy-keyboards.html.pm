a78f571209773923df0b4511c2784b5ea9364018
Kenny Shen <kenny@machinesung.com>
2018-11-14 22:31:29 +0800

    Update (keyboards)

--- src/others/diy-keyboards.html.pm
+++ src/others/diy-keyboards.html.pm
@@ -4,6 +4,13 @@
 ◊h2{Adventures in typing}
 ◊m-article{
   ◊p{◊em{Rewriting in progress, check back again later}}
+  ◊h3{Typing for work}
+  ◊p{Like a lot of folks, I didn't really start with touch typing proper but rather adopted a "hunt and peck" approach. But sometime early 2017, I decided to work on changing that due to some mild symptoms of RSI and also to increase my typing accuracy. I suffered a month of painful transition before gaining some success touch typing at about 60wpm in ◊a[#:href "https://colemak.com/"]{Colemak}. Since then, with the fact that I do type for a living, I've reached a stable rate of 75 to 80 wpm.}
+  ◊p{I've since moved away from the standard keyboards to split◊string->symbol{dash}ortholinear◊string->symbol{dash}tilted variants, my main workhorses being the ◊a[#:href "https://www.kinesis-ergo.com/shop/advantage2/"]{Kinesis Advantage} at the office and my ◊a[#:href "https://keeb.io/collections/frontpage/products/iris-keyboard-split-ergonomic-keyboard?variant=8034004860958"]{Iris} at home.}
+  ◊h3{Typing for fun}
+  ◊p{I like taking breaks going to ◊a[#:href "https://play.typeracer.com/"]{Typeracer}. It's a good way to practice words that might not necessarily come under my daily programming vocabulary. I'm almost at a thousand races now at the time of writing this entry.}
+  ◊p{I also enjoyed ◊a[#:href "http://epistorygame.com/"]{Epistory ◊string->symbol{dash} Typing Chronicles}. If you like a simple action and adventure game that makes you type this is definitely one to go for.}
+  ◊h3{Some of my keyboards}
   ◊figure[#:class "image" #:style "width: 75%; padding: 1rem;"]{
     ◊img[#:src "/assets/images/iris.jpg"]{}
     The Iris, with Kailh box reds and Tealios for the thumb clusters.
